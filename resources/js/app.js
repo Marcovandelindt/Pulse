442fbfdc1a52e63713d59741087b01239a8aaf15
Marco <marcovdlindt1998@gmail.com>
2026-08-13 21:41:59 +0200

    Revert "Add Music feature with Spotify integration"
    This reverts commit 9b694a2defde80c7c225c75102da361264bb8ba6.

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,13 +2,11 @@ import Alpine from 'alpinejs';
 import { initCharts } from './modules/charts.js';
 import { registerTvComponents } from './modules/tv.js';
 import { registerMovieComponents } from './modules/movies.js';
-import { registerMusicComponents } from './modules/music.js';
 
 window.Alpine = Alpine;
 
 registerTvComponents();
 registerMovieComponents();
-registerMusicComponents();
 
 Alpine.start();
 
